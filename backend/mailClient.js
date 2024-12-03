5ff90fcfc849ffb6bd23aa3557c24e90618c6260
vicsaa01 <victorsaakes@gmail.com>
2024-12-03 14:08:21 +0100

    #002 - using .env

--- backend/mailClient.js
+++ backend/mailClient.js
@@ -1,5 +1,6 @@
 const FormData = require('form-data');
 const Mailgun = require('mailgun.js');
+require('dotenv').config()
 
 const express = require('express')
 const cors = require('cors')
@@ -7,7 +8,7 @@ const cors = require('cors')
 // Setup Mailgun client
 
 const mailgun = new Mailgun(FormData);
-const client = mailgun.client({username: 'api', key: ''}) // DON'T PASTE KEY, USE ENV
+const client = mailgun.client({username: 'api', key: process.env.MAILGUN_APIKEY}) // DON'T PASTE KEY, USE ENV
 
 // Run Express app
 
@@ -25,8 +26,8 @@ app.listen(port, () => {
 // API route
 
 app.post('/send-mail', (req, res) => {
-    client.messages.create('<domain>', {
-        from: "postmaster@<domain>",
+    client.messages.create(process.env.MAILGUN_DOMAIN, {
+        from: "postmaster@" + process.env.MAILGUN_DOMAIN,
         to: ["victorsaakes@gmail.com"],
         subject: 'Message from ' + req.body.name,
         text: "New message",
